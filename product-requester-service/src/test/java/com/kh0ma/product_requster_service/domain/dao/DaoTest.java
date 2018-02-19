132852eb797a6852a651c9b8e0efddd8e9db9f57
kh0ma <khomenko.dp@gmail.com>
2018-02-19 19:24:39 +0200

    add tests for DAO

---
+++ product-requester-service/src/test/java/com/kh0ma/product_requster_service/domain/dao/DaoTest.java
@@ -0,0 +1,49 @@
+package com.kh0ma.product_requster_service.domain.dao;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * @author Olexander Khomenko
+ */
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class DaoTest<T,PK> {
+
+    public abstract Dao<T,PK> getDao();
+
+    public abstract Collection<? extends T> getTestingData();
+
+    @Ignore
+    @Test
+    public void save() {
+    }
+
+    @Ignore
+    @Test
+    public void findOne() {
+    }
+
+    @Ignore
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void findAll() {
+        Collection<? extends T> all = getDao().findAll();
+
+        assertArrayEquals(getTestingData().toArray(),all.toArray());
+    }
+}
---
+++ product-requester-service/src/test/java/com/kh0ma/product_requster_service/domain/dao/ProductDaoTest.java
@@ -0,0 +1,72 @@
+package com.kh0ma.product_requster_service.domain.dao;
+
+import com.kh0ma.product_requster_service.domain.entity.Product;
+import com.kh0ma.product_requster_service.domain.entity.ProductBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * @author Olexander Khomenko
+ */
+
+public class ProductDaoTest extends DaoTest<Product,Long> {
+
+    @Autowired
+    private ProductDao dao;
+
+    @Override
+    public Dao<Product, Long> getDao() {
+        return dao;
+    }
+
+    @Override
+    public Collection<? extends Product> getTestingData() {
+        return Arrays.asList(
+                ProductBuilder.aProduct()
+                        .withId(1L)
+                        .withName("Apple")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(12.99))
+                        .withProductCategoryId(1L)
+                        .build(),
+                ProductBuilder.aProduct()
+                        .withId(2L)
+                        .withName("Orange")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(10.87))
+                        .withProductCategoryId(1L)
+                        .build(),
+                ProductBuilder.aProduct()
+                        .withId(3L)
+                        .withName("Papaya")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(39.78))
+                        .withProductCategoryId(1L)
+                        .build(),
+                ProductBuilder.aProduct()
+                        .withId(4L)
+                        .withName("Potato")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(2.99))
+                        .withProductCategoryId(2L)
+                        .build(),
+                ProductBuilder.aProduct()
+                        .withId(5L)
+                        .withName("Onion")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(1.59))
+                        .withProductCategoryId(2L)
+                        .build(),
+                ProductBuilder.aProduct()
+                        .withId(6L)
+                        .withName("Cucumber")
+                        .withDescription("Some Test Description")
+                        .withPrice(BigDecimal.valueOf(2.19))
+                        .withProductCategoryId(2L)
+                        .build()
+        );
+    }
+}
